510ccfd7581fce0715c255693ee748e21e9d5c7c
Trishna Pattanaik <trishna.PATTANAIK@arcstone.co>
2025-04-27 00:24:53 +0530

    add id for view model

--- Service/RoomManager.Application/Queries/Models/RoomModel.cs
+++ Service/RoomManager.Application/Queries/Models/RoomModel.cs
@@ -2,6 +2,7 @@
 {
     public class RoomModel
     {
+        public int Id { get; set; }
         public string Number { get; set; }
         public string Type { get; set; }
         public RoomAvailabilityModel Availability { get; set; }
